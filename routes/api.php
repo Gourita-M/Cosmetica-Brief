d524a0e807ba655796c7a915bf2d058704e90fee
Mouad Gourita <mouad.gourita@gmail.com>
2026-03-16 14:17:06 +0000

    feat: Implement product listing and detail API endpoints, including category relationships for products.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,6 @@ Route::middleware('auth:api')->get('/profile', function(Request $request) {
 });
 
 Route::GET('/login', [AuthController::class, 'login']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{slug}', [ProductController::class, 'show']);
