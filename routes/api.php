f2cfc5279ef7a3de8edadf40426bfcf46d721b65
Mouad Gourita <mouad.gourita@gmail.com>
2026-03-16 12:57:40 +0000

    feat: Implement API authentication with JWT and Sanctum, and add initial database factories, seeders, and policies.

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::POST('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->get('/profile', function(Request $request) {
+    return auth()->user();
+});
+
+Route::GET('/login', [AuthController::class, 'login']);
