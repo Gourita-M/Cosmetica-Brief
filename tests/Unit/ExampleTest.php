dc3f4976b721cf884c7103f954eb0d555d4f5fa8
Mouad Gourita <mouad.gourita@gmail.com>
2026-03-16 10:58:17 +0000

    fix: added role string to users migration

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,30 @@
-<?php
+<?php 
 
-namespace Tests\Unit;
+Class Student{
+    private string $name;
+    private array $grades = [];
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    public function __construct($name)
     {
-        $this->assertTrue(true);
+        $this->name = $name;
+    }
+
+    public function addGrade($grade)
+    {
+        $this->grades[] = $grade;
+    }
+
+    public function getAverage()
+    {
+        $sum = 0;
+        $count = count($this->grades);
+
+        foreach($this->grades as $grade){
+            $sum += $grade;
+        }
+
+        $average = $sum / $count;
+
+        return $average;
     }
 }
