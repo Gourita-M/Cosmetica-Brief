e79f824fe94f2edcc5f450f47c1cbe29fa25b838
Mouad Gourita <mouad.gourita@gmail.com>
2026-03-17 12:43:41 +0000

    feat: Add user case diagram and API route for creating orders.

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::get('/orders', [OrderController::class, 'index']);
+    Route::post('/orders', [OrderController::class, 'store']);
     Route::get('/orders/{order}', [OrderController::class, 'show']);
     Route::post('/orders/{order}/cancel', [OrderController::class, 'cancel']);
 
